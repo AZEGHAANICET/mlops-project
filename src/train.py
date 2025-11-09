df56e1da9101a15e25e5a841904bc1eab6c2e746
Anicet AZEGHA <loicanicetazegha@gmail.com>
2025-11-09 04:09:48 +0100

    Experiment with max_iter=500

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ y = df["species"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train the model
-model = LogisticRegression(solver="lbfgs", max_iter=200)
+model = LogisticRegression(solver="lbfgs", max_iter=500)
 model.fit(X_train, y_train)
 
 
